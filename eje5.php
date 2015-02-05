b4c9973b2c0a5109aaec8372695563f11c7cc39a
Nicolle Gonzalez <gnicolle93@gmail.com>
2015-02-04 19:46:28 -0600

    Ehercicio con funciones

---
+++ eje5.php
@@ -0,0 +1,36 @@
+<?php
+ $contenidoSesion="";
+ function agregarAession($data){
+    return "AS Contenido:" . $data; 
+ }
+ function eliminarSession($data){
+     return "ES Contenido:" . $data; 
+ }
+ 
+ if(isset($_POST["btnAdd"])){
+    $contenidoSesion=agregarAession($_POST["txtData"]);
+ }
+ if(isset($_POST["btnSubs"])){
+    $contenidoSesion= eliminarSession($_POST["txtData"]);
+ }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Funciones y Sesion</title>
+    <meta charset="utf-8"/>
+</head>
+<body>
+    <h1>Funciones y el Arreglo Sesion</h1>
+    <form action="eje5.php" method="POST"/>
+        <label for="txtData">Texto de un blog</label>
+        <input type="text" id="txtData" name="txtData" />
+        </br>
+        <input type="submit" id="btnAdd" name="btnAdd" value="Agregar a la sesion" />
+        <input type="submit" id="btnSubs" name="btnSubs" value="Eliminar de la sesion" />
+    </form>
+    <div>
+        <?php echo $contenidoSesion; ?>
+    </div>
+</body>
+</html>
